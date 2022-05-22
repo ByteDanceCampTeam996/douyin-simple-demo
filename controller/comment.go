b09c7326779614a1a1cf01efe0683ae4e2c2f660
kjctar <2412864341@qq.com>
2022-05-22 13:14:04 +0800

    init

--- controller/comment.go
+++ controller/comment.go
@@ -1,9 +1,23 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
+
+type comment struct {
+	Id         int64 `gorm:"-;primary_key;AUTO_INCREMENT"`
+	Uid        int64
+	Vid        int64
+	Content    string
+	CreateDate string
+}
+
+//var comments []comment
 
 type CommentListResponse struct {
 	Response
@@ -11,10 +25,86 @@ type CommentListResponse struct {
 }
 
 // CommentAction no practical effect, just check if token is valid
+func CommentInsert(ct comment) {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	// 延迟关闭数据库
+	defer db.Close()
+
+	db.Create(&ct)
+
+}
+func CommentFindByVid(vid int64) (comments []comment, err error) {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	// 延迟关闭数据库
+	defer db.Close()
+
+	res := db.Where("vid=?", vid).Find(&comments)
+	err = res.Error
+	return
+}
+func CommentDeleteById(id int64) (err error) {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	// 延迟关闭数据库
+	defer db.Close()
+
+	res := db.Where("id=?", id).Delete(comment{})
+	err = res.Error
+	return
+}
 func CommentAction(c *gin.Context) {
 	token := c.Query("token")
 
 	if _, exist := usersLoginInfo[token]; exist {
+		action_type, err := strconv.Atoi(c.Query("action_type"))
+		if err != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "action_type err"})
+			return
+		}
+		uid := usersLoginInfo[token].Id
+		vid, err2 := strconv.ParseInt(c.Query("video_id"), 10, 64)
+		if err2 != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "video_id format err"})
+			return
+		}
+		// var id int64 = 1
+		// if len(comments) > 0 {
+		// 	id = comments[len(comments)-1].Uid + 1
+		// }
+
+		// if action_type == 1 {
+		// 	comments = append(comments, comment{
+		// 		id,
+		// 		uid,
+		// 		vid,
+		// 		c.Query("comment_text"),
+		// 		time.Now().Format("12-05"),
+		// 	})
+		// }
+		if action_type == 1 {
+			var cmt comment
+			cmt.Content = c.Query("comment_text")
+			cmt.CreateDate = time.Now().Format("12-05")
+			cmt.Uid = uid
+			cmt.Vid = vid
+			CommentInsert(cmt)
+		} else if action_type == 2 {
+			id, err3 := strconv.ParseInt(c.Query("video_id"), 10, 64)
+			if err3 != nil {
+				c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "video_id format err"})
+				return
+			}
+			CommentDeleteById(id)
+		}
+
 		c.JSON(http.StatusOK, Response{StatusCode: 0})
 	} else {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
@@ -23,8 +113,35 @@ func CommentAction(c *gin.Context) {
 
 // CommentList all videos have same demo comment list
 func CommentList(c *gin.Context) {
+
+	var com []Comment
+	vid, err2 := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	if err2 != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "video_id format err"})
+		return
+	}
+	// for i := 0; i < len(comments); i++ {
+	// 	if comments[i].Vid == vid {
+	// 		com = append(com, Comment{
+	// 			comments[i].Id,
+	// 			usersLoginInfo[token],
+	// 			comments[i].Content,
+	// 			comments[i].CreateDate,
+	// 		})
+	// 	}
+	// }
+	comments, _ := CommentFindByVid(vid)
+	for i := 0; i < len(comments); i++ {
+		user := DemoUser //需要提供接口 ： getUserById(id int64)(User,error)
+		com = append(com, Comment{
+			comments[i].Id,
+			user,
+			comments[i].Content,
+			comments[i].CreateDate,
+		})
+	}
 	c.JSON(http.StatusOK, CommentListResponse{
 		Response:    Response{StatusCode: 0},
-		CommentList: DemoComments,
+		CommentList: com,
 	})
 }
--- controller/favorite.go
+++ controller/favorite.go
@@ -1,8 +1,12 @@
 package controller
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 // FavoriteAction no practical effect, just check if token is valid
@@ -10,6 +14,18 @@ func FavoriteAction(c *gin.Context) {
 	token := c.Query("token")
 
 	if _, exist := usersLoginInfo[token]; exist {
+		uid := usersLoginInfo[token].Id
+		vid, err2 := strconv.ParseInt(c.Query("video_id"), 10, 64)
+		if err2 != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "video_id format err"})
+			return
+		}
+		err3 := SetFavorite(uid, vid)
+		if err3 != nil {
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: " relate  err"})
+			return
+		}
+
 		c.JSON(http.StatusOK, Response{StatusCode: 0})
 	} else {
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
@@ -18,10 +34,84 @@ func FavoriteAction(c *gin.Context) {
 
 // FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
+	token := c.Query("token")
+	uid := usersLoginInfo[token].Id
+
 	c.JSON(http.StatusOK, VideoListResponse{
 		Response: Response{
 			StatusCode: 0,
 		},
-		VideoList: DemoVideos,
+		VideoList: GetFavoriteList(uid, c.Query("token")),
 	})
+
+}
+
+//server
+
+func SetFavorite(uid int64, vid int64) error {
+	return FavoriteUpdate(uid, vid)
+}
+
+func GetFavoriteList(uid int64, token string) []Video {
+	video_ids, err := FavoriteVid(uid)
+	if err != nil {
+		println(video_ids)
+
+	}
+
+	var videos = []Video{
+		{
+			Id:            1,
+			Author:        usersLoginInfo[token],
+			PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+			CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+			FavoriteCount: 1,
+			CommentCount:  4,
+			IsFavorite:    true,
+		},
+	}
+	return videos
+
+}
+
+//dao
+
+type Favorite struct {
+	Uid    int64
+	Vid    int64
+	Status int
+}
+
+func FavoriteVid(uid int64) (vid_list []int64, er error) {
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	//db.LogMode(true)
+	res := db.Table("favorites").Where("uid=?", uid).Select("vid").Find(&vid_list)
+	er = res.Error
+	return
+}
+func FavoriteUpdate(uid int64, vid int64) error {
+
+	db, err := gorm.Open("mysql", "root:123456@(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	u := Favorite{}
+	res := db.Where("uid=?", uid).Where("vid=?", vid).Find(&u)
+	if res.RowsAffected == 0 {
+		db.Create(Favorite{uid, vid, 1})
+	} else if u.Status == 1 {
+		db.Model(Favorite{}).Where("uid=?", uid).Where("vid=?", vid).Update("status", 0)
+	} else {
+		db.Model(Favorite{}).Where("uid=?", uid).Where("vid=?", vid).Update("status", 1)
+	}
+
+	return nil
 }
